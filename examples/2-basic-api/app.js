551e988765dad70e87096b6a1ed579205bf02fef
Annie Huddleston <ahuddleston@remedyhealthmedia.com>
2021-12-02 16:11:29 -0500

    setup basic api for ex. 2

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,21 @@
 const result = document.querySelector('.result')
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios.get('/api/2-basic-api')
+    result.innerHTML = data.map(product => (`
+    <article class="product">
+      <img src=${product.image.url} alt=${product.name} />
+      <div class="info">
+        <h5>${product.name}</h5>
+        <h5 class="price">$${product.price}</h5>
+      </div>
+    </article>
+`)).join(' ');
+  } catch (err) {
+    console.log(err.response)
+    result.innerHTML = 'Please try again later'
+  }
+}
+
+fetchData()
